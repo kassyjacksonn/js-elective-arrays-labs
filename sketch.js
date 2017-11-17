277311582e772f252abfd267fb26a8a4512d7e12
Kassandra Fuentes <kassandraf32528@students.laalliance.org>
2017-11-17 17:23:32 +0000

    Finished my project.

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+function setup() {
+    createCanvas(800, 600)
+}
+
+function draw() {
+  noFill()
+  strokeWeight(4)
+  ellipse(50, 50, 80, 80);
+  ellipse(100, 100, 80, 80);
+  ellipse(150, 50, 80, 80);
+  ellipse(200, 100, 80, 80);
+  ellipse(250, 50, 80, 80);
+  
+}
